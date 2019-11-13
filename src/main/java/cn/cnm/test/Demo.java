d980da71942be9e2c0d80096463a3c5ac67ad9ef
ziyaliuye <51473277+ziyaliuye@users.noreply.github.com>
2019-11-13 18:19:05 +0800

    完善代码...

--- src/main/java/cn/cnm/test/Demo.java
+++ src/main/java/cn/cnm/test/Demo.java
@@ -2,6 +2,8 @@ package cn.cnm.test;
 
 import cn.cnm.mapper.FlowerMapper;
 import cn.cnm.pojo.Flower;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -21,5 +23,12 @@ public class Demo {
 
         Flower insertFlower = new Flower(6, "葵花", 99.9, "中国");
         flowerMapper.insert(insertFlower);
+
+        /* 分页查询 */
+        // 定义一个分页查询类, 第一个参数是查询的页码， 第二个参数是多个条记录为一页
+        Page<Flower> userPage = new Page<>(3, 2);//参数一是当前页，参数二是每页个数
+        IPage<Flower> iPage = flowerMapper.selectPage(userPage, null);
+        // getRecords()就是返回对应的记录
+        System.out.println(iPage.getRecords());
     }
 }
