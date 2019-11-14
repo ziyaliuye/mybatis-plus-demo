55a69bff46b8ac9ee2635fe0af41dd50f760e00e
ziyaliuye <171242039@qq.com>
2019-11-14 22:30:34 +0800

    first commit...

--- src/main/java/cn/cnm/test/GenerationCode.java
+++ src/main/java/cn/cnm/test/GenerationCode.java
@@ -18,22 +18,27 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
  */
 public class GenerationCode {
     public static void main(String[] args) {
-        //1. 全局配置
+        // 全局配置
         GlobalConfig config = new GlobalConfig();
-        config.setActiveRecord(true) // 是否支持AR模式
-                .setAuthor("lele") // 作者
+        // 是否支持AR模式
+        config.setActiveRecord(true)
+                // 设置注释上的作者名
+                .setAuthor("lele")
                 //.setOutputDir("D:\\IDEA Space\\mybatis-plus-demo\\src\\main\\java") // 生成路径， 选择项目的java目录
                 .setOutputDir("D:\\code")
-                .setFileOverride(true)  // 是否覆盖文件
-                .setIdType(IdType.AUTO) // 主键策略，自增
-                .setServiceName("%sService")  // 默认生成会带一个I， 这里设置将生成的service接口的名字的首字母是否为I去掉
-                // IEmployeeService
+                // 是否覆盖文件
+                .setFileOverride(true)
+                // 主键策略，自增
+                .setIdType(IdType.AUTO)
+                // 默认生成第一个字母带一个I， 这里设置将生成的service接口的名字的首字母是否为I去掉
+                .setServiceName("%sService")
                 .setBaseResultMap(true)
                 .setBaseColumnList(true);
 
-        //2. 数据源配置
+        // 数据源配置
         DataSourceConfig dsConfig = new DataSourceConfig();
-        dsConfig.setDbType(DbType.MYSQL)  // 设置数据库类型
+        // 设置数据库类型
+        dsConfig.setDbType(DbType.MYSQL)
                 .setDriverName("com.mysql.cj.jdbc.Driver")
                 .setUrl("jdbc:mysql://192.168.58.128:3306/ssm?serverTimezone=UTC")
                 .setUsername("root")
@@ -41,15 +46,21 @@ public class GenerationCode {
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
-        strategy.setCapitalMode(true); //全局大写命名
-        strategy.setNaming(NamingStrategy.underline_to_camel); // 数据库表映射到实体的命名策略
+        //全局大写命名
+        strategy.setCapitalMode(true);
+        // 数据库表映射到实体的命名策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        strategy.setTablePrefix("tb_");  // 设置表名前缀,
-        strategy.setInclude("airport");  // 生成的表， 多表用数组
-        // 自定义继承的Entity类全称，带包名
+        // 设置表名前缀
+        strategy.setTablePrefix("tb_");
+        // 生成的表， 多表用数组
+        strategy.setInclude("airport");
+        // 自定义继承的实体类类全称，带包名， 这里暂不需要
         // strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
-        strategy.setEntityLombokModel(true); // 是否为lombok模型
-        strategy.setRestControllerStyle(true); // 生成<code>@RestController</code> 控制器
+        // 是否为lombok模型
+        strategy.setEntityLombokModel(true);
+        // 生成<code>@RestController</code> 控制器
+        strategy.setRestControllerStyle(true);
 
         // 包名策略配置
         PackageConfig pkConfig = new PackageConfig();
@@ -61,15 +72,14 @@ public class GenerationCode {
                 // .setModuleName("模块名")
                 .setXml("mapper");
 
-
-        //5. 整合配置
+        // 整合配置
         AutoGenerator ag = new AutoGenerator();
         ag.setGlobalConfig(config)
                 .setDataSource(dsConfig)
                 .setStrategy(strategy)
                 .setPackageInfo(pkConfig);
 
-        //6. 执行
+        // 执行
         ag.execute();
     }
 }
